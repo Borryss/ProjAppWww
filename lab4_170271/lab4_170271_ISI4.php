f566648df609a8315bd8ab86e4190ffa3f9af241
Borryss <115550381+Borryss@users.noreply.github.com>
2024-10-24 18:26:03 +0200

    Add files via upload

---
+++ lab4_170271/lab4_170271_ISI4.php
@@ -0,0 +1,19 @@
+<?php
+
+    include 'testik.php';
+    $s = require_once('testik.php');
+    echo  $s . '<br/>';
+    echo "A $color $fruit" . '<br/>';
+    $numba = 69;
+
+    if ($numba == $numba)
+        echo "A $color $fruit" . '<br/>';
+
+    $nr_indeksu = '170271';
+    $nrGrupy = 'ISI4';
+
+    echo 'Borys Kutsenko' . $nr_indeksu . '<br/>' . 'grupa' . $nrGrupy . '<br/>';
+
+    echo 'Zastosowanie metody include() <br/ >';
+
+?>
